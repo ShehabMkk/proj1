7c785462e72a9864b685856c14f013f406ded721
ShehabMkk <shehab.mkk2004@gmail.com>
2023-07-05 14:45:50 +0300

    1

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,36 @@
+
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
-
+import 'package:get/get.dart';
+import 'package:whatclone/view/screen/home.dart';
 void main() {
   runApp(const MyApp());
 }
-
+ 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
+ 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: FacebookApp(),
+      home: ChatLiveWeb(),
     );
   }
 }
 
-class FacebookApp extends StatelessWidget {
-  const FacebookApp({Key? key}) : super(key: key);
+class ChatLiveWeb extends StatelessWidget {
+  const ChatLiveWeb({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("facebook", style: TextStyle(color: Colors.blueAccent, fontSize: 27, fontWeight:  FontWeight.w700)),
-        centerTitle: true,
-        leading: IconButton(
-          icon: Icon(Icons.menu, color: Colors.blueAccent, size: 30, ),
-          onPressed: () {},
-        ),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.message, color: Colors.blueAccent, size: 30,), 
-            onPressed: () {}),
-          IconButton(
-            icon: Icon(Icons.search, color: Colors.blueAccent, size: 30,),
-            onPressed: () {},
-          ),
-        ],
-        
-        backgroundColor: Colors.white,
-        elevation: 20,
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(backgroundColor: Color.fromARGB(255, 190, 54, 244)),
+
       ),
-    
-      
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.add),
-      ),
-    
-    
+      home: Home(),
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:proj1/main.dart';
+import 'package:whatclone/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
